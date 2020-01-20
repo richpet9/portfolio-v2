8977109c2fa3bf38023536941df850431d715142
richpet9 <richpet9@gmail.com>
2020-01-20 18:07:57 -0500

    visual adjustments to resume

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -3,16 +3,16 @@ import SideNav from '../components/SideNav/';
 import { Experience, Education, Skills, Honors } from '../components/Resume/';
 
 const About = props => {
-  return (
-    <main id="app-container">
-      <div id="resume-container">
-        <Education />
-        <Experience />
-        <Skills />
-        <Honors />
-      </div>
-    </main>
-  );
+    return (
+        <main id="app-container">
+            <div id="resume-container">
+                <Education />
+                <Experience />
+                <Skills />
+                <Honors />
+            </div>
+        </main>
+    );
 };
 
 export default About;
