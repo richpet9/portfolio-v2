f50f476a6ff84ea84187ec381be68174aa3f7e07
richpet9 <richpet9@gmail.com>
2020-01-28 01:56:54 -0500

    responsive blog posts

--- frontend/src/components/SingleBlogPost/index.js
+++ frontend/src/components/SingleBlogPost/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Marked from 'marked';
 
 import SanitizedHTML from 'react-sanitized-html';
@@ -9,24 +9,35 @@ import './index.css';
 const sanitizeAllowTags = ['a', 'h1', 'h2', 'h3', 'p', 'i', 'em', 'b', 'strong'];
 
 const SingleBlogPost = ({ post }) => {
+    const [bgWidth, setBgWidth] = useState(window.innerWidth);
+
+    const handleResize = () => {
+        setBgWidth(window.innerWidth);
+    };
+    window.onresize = handleResize;
+
     return (
         <div className="single-blog-post">
-            <h1 className="blog-post-title">{post.name}</h1>
-            <div className="blog-post-meta">
-                <div className="blog-post-cat">
-                    {post.category
-                        ? post.category.includes('-')
-                            ? post.category.split('-').map(side => {
-                                  return side[0].toUpperCase() + side.slice(1) + ' ';
-                              })
-                            : post.category
-                        : 'No Category'}
+            <div className="container">
+                <h1 className="blog-post-title">{post.name}</h1>
+                <div className="blog-post-meta">
+                    <div className="blog-post-cat">
+                        {post.category
+                            ? post.category.includes('-')
+                                ? post.category.split('-').map(side => {
+                                      return side[0].toUpperCase() + side.slice(1) + ' ';
+                                  })
+                                : post.category
+                            : 'No Category'}
+                    </div>
+                    <span className="vline"></span>
+                    <div className="blog-post-date">{dateFormat(post.date)}</div>
                 </div>
-                <span className="vline"></span>
-                <div className="blog-post-date">{dateFormat(post.date)}</div>
             </div>
-            <div className="blog-post-img" style={{ backgroundImage: "url('" + post.img_url + "')" }}></div>
-            <SanitizedHTML className="blog-post-long-body" html={Marked(post.body)} allowedTags={sanitizeAllowTags} />
+
+            <div className="blog-post-img" style={{ backgroundImage: "url('" + post.img_url + "')", width: bgWidth }}></div>
+
+            <SanitizedHTML className="blog-post-long-body container" html={Marked(post.body)} allowedTags={sanitizeAllowTags} />
         </div>
     );
 };
