5704543db0c70aed93298bd822fb8ae03461cd38
richpet9 <richpet9@gmail.com>
2020-01-30 11:01:49 -0500

    pm2 config update

--- pm2.config.js
+++ pm2.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  apps : [{
-    name      : 'portfolio-v2',
-    script    : 'backend/server.js',
-    node_args : '-r dotenv/config'
-  }],
-}
+    apps: [
+        {
+            name: 'portfolio-v2',
+            script: 'backend/server.js',
+            node_args: '-r dotenv/config',
+            watch: ['frontend/public'],
+            watch_delay: 1000,
+            ignore_watch: ['node_modules']
+        }
+    ]
+};
