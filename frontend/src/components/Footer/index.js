f6db26672b55a03d31143addcfe40f4216379f8d
richpet9 <richpet9@gmail.com>
2020-03-08 17:47:01 -0400

    copyright

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -3,21 +3,21 @@ import React from 'react';
 import './index.css';
 
 const Footer = () => {
-  return (
-    <footer>
-      <ul>
-        <li>
-          <a href="https://github.com/richpet9/portfolio-v2">Github</a>
-        </li>
-        <li>
-          <a href="mailto:richpet9@gmail.com">richpet9@gmail.com</a>
-        </li>
-        <li>
-          <p id="footer-copyright">&copy; 2019 Richard Petrosino</p>
-        </li>
-      </ul>
-    </footer>
-  );
+    return (
+        <footer>
+            <ul>
+                <li>
+                    <a href="https://github.com/richpet9/portfolio-v2">Github</a>
+                </li>
+                <li>
+                    <a href="mailto:richpet9@gmail.com">richpet9@gmail.com</a>
+                </li>
+                <li>
+                    <p id="footer-copyright">&copy; 2020 Richard Petrosino</p>
+                </li>
+            </ul>
+        </footer>
+    );
 };
 
 export default Footer;
