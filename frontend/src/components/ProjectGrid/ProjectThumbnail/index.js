a796e8283d0217bc3a2b77bf9c3bd95f710c2bf4
richpet9 <richpet9@gmail.com>
2020-04-20 14:21:04 -0400

    better homescreen styling

--- frontend/src/components/ProjectGrid/ProjectThumbnail/index.js
+++ frontend/src/components/ProjectGrid/ProjectThumbnail/index.js
@@ -4,13 +4,22 @@ import { Link } from 'react-router-dom';
 import './index.css';
 
 const Item = ({ url, name, tags, desc, thumbnail }) => {
+    tags = tags.split(',');
     return (
         <div className="item">
             <div className="item-bg" style={{ backgroundImage: thumbnail ? 'url("' + thumbnail + '")' : '' }}></div>
             <Link className="item-link" to={url} title={desc}>
                 <div className="item-meta">
                     <h1 className="item-name">{name}</h1>
-                    <span className="item-tags">{tags}</span>
+                    <span className="item-tags">
+                        {tags.map((tag) => {
+                            return (
+                                <span className="tag" key={tag}>
+                                    {tag[0].toUpperCase() + tag.substring(1)}
+                                </span>
+                            );
+                        })}
+                    </span>
                     <p>{desc}</p>
                 </div>
             </Link>
