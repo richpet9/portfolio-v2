95347603f25bcd9ad54cf73124451f34b4672252
richpet9 <richpet9@gmail.com>
2020-04-22 14:41:28 -0400

    working filter

--- frontend/src/components/Filter/index.js
+++ frontend/src/components/Filter/index.js
@@ -6,6 +6,7 @@ import { getQueryParams } from '../../util';
 import './index.css';
 
 const Filter = (props) => {
+    const tags = ['Print', 'PHP', 'Javascript', 'React', 'Brand', 'Logo', 'Typescript', 'Game'];
     const queryParams = getQueryParams(props.location.search);
 
     let activeCategory = [];
@@ -18,10 +19,76 @@ const Filter = (props) => {
         activeTags = queryParams.tags.split(',');
     }
 
-    const tags = ['PHP', 'Javascript', 'React', 'Brand', 'Logo', 'Typescript', 'Game'];
-
     const updateCategory = (category) => {
-        props.history.push('/?hi');
+        category = category.toLowerCase();
+        // Check if any categories are currently active
+        if (activeCategory.length > 0) {
+            // Check if the active category includes this category
+            if (activeCategory.includes(category)) {
+                // If it does, remove this category (we unchecked a box)
+                activeCategory = activeCategory.filter((item) => item !== category);
+            } else {
+                // If it doesnt, add this category (we checked a box)
+                activeCategory.push(category);
+            }
+        } else {
+            // If there are no active categories, make this the active only active category
+            activeCategory = [category];
+        }
+
+        // Update hash params here
+        updateHashParams(activeCategory, activeTags);
+    };
+
+    const updateTags = (tag) => {
+        tag = tag.toLowerCase();
+        // Check if any categories are currently active
+        if (activeTags.length > 0) {
+            // Check if the active category includes this category
+            if (activeTags.includes(tag)) {
+                // If it does, remove this category (we unchecked a box)
+                activeTags = activeTags.filter((item) => item !== tag);
+            } else {
+                // If it doesnt, add this category (we checked a box)
+                activeTags.push(tag);
+            }
+        } else {
+            // If there are no active categories, make this the active only active category
+            activeTags = [tag];
+        }
+
+        // Update hash params here
+        updateHashParams(activeCategory, activeTags);
+    };
+
+    const updateHashParams = (activeCategory, activeTags) => {
+        let queryStr = '/?',
+            tagStr,
+            catStr;
+
+        if (activeCategory && activeCategory.length > 0) {
+            catStr = 'category=' + activeCategory.join(',');
+        }
+
+        if (activeTags && activeTags.length > 0) {
+            tagStr = 'tags=' + activeTags.join(',');
+        }
+
+        if (catStr && tagStr) {
+            queryStr += [catStr, tagStr].join('&');
+        } else if (catStr) {
+            queryStr += catStr;
+        } else if (tagStr) {
+            queryStr += tagStr;
+        } else {
+            queryStr = '';
+        }
+
+        // Update the browser's URL
+        props.history.push(queryStr);
+
+        // Tell the root of the page to update the projects
+        props.updateFilter(activeCategory, activeTags);
     };
 
     return (
@@ -39,7 +106,7 @@ const Filter = (props) => {
                 <li className="filter-label">TAGS</li>
                 {tags.map((tag) => (
                     <li key={tag} className="filter-checkbox">
-                        <Checkbox value={tag} preChecked={activeTags.includes(tag.toLowerCase())} />
+                        <Checkbox value={tag} preChecked={activeTags.includes(tag.toLowerCase())} onToggle={updateTags.bind(this, tag)} />
                     </li>
                 ))}
             </ul>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,7 +5,7 @@ import Project from '../components/Project';
 
 const Home = ({ match }) => {
     const [projects, setProjects] = useState(null);
-    const [projectId, setProjectId] = useState(null);
+    const [sortedProjects, setSortedProjects] = useState(null);
 
     const fetchProjects = (id) => {
         const url = '/api/projects' + (id ? `/${id}` : '') + '?limit=' + 10;
@@ -23,23 +23,51 @@ const Home = ({ match }) => {
             });
     };
 
-    const toggleFilter = (key, value) => {
-        console.log(key, value);
+    const updateFilter = (activeCategory, activeTags) => {
+        let newProjects = projects.filter((project) => {
+            let projectTags = project.tags.toLowerCase().split(',');
+            let catIntersect = projectTags.filter((tag) => activeCategory.includes(tag));
+            let tagIntersect = projectTags.filter((tag) => activeTags.includes(tag));
+
+            if (activeCategory.length > 0 && activeTags.length > 0) {
+                if (catIntersect.length > 0 && tagIntersect.length > 0) {
+                    return true;
+                }
+            } else if (activeCategory.length > 0) {
+                if (catIntersect.length > 0) {
+                    return true;
+                }
+            } else if (activeTags.length > 0) {
+                if (tagIntersect.length > 0) {
+                    return true;
+                }
+            } else {
+                return true;
+            }
+        });
+
+        setSortedProjects(newProjects.length > 0 ? newProjects : null);
     };
 
     useEffect(() => {
-        if (match.params.id) setProjectId(match.params.id);
-        else setProjectId(null);
-
         fetchProjects(match.params.id).then((res) => {
             setProjects(res);
+            setSortedProjects(res);
         });
-    }, [match.params]);
+    }, [match.params.id]);
 
     return (
         <main id="app-container" style={{ display: 'flex' }}>
-            {projects && projects.length > 1 ? <Filter toggleFilter={toggleFilter} /> : ''}
-            {projects ? projects.length > 1 ? <ProjectContainer items={projects} /> : <Project item={projects[0]} /> : 'Error retrieving projects.'}
+            {projects && projects.length > 1 ? <Filter updateFilter={updateFilter} /> : ''}
+            {sortedProjects ? (
+                sortedProjects.length > 1 ? (
+                    <ProjectContainer items={sortedProjects} />
+                ) : (
+                    <Project item={sortedProjects[0]} />
+                )
+            ) : (
+                'Error retrieving projects.'
+            )}
         </main>
     );
 };
