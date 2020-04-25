aeaead10beb0b32452b4c9e8ce8f01099bd4460c
richpet9 <richpet9@gmail.com>
2020-04-24 20:02:04 -0400

    functional contact

--- frontend/src/components/Input/index.js
+++ frontend/src/components/Input/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 import './index.css';
 
-const Input = (props) => {
+const Input = (props, ref) => {
     return (
         <div className="input-container">
             {props.label && <span className="input-label">{props.label}</span>}
-            {props.type === 'text' && <input type={props.type} className="input-text" value={props.value} />}
-            {props.type === 'textarea' && <textarea className="input-text" />}
-            {props.type === 'number' && <input type={props.type} className="input-text" value={props.value} min={props.min} max={props.max} />}
+            {props.type === 'text' && <input ref={ref} type={props.type} className="input-text" value={props.value} />}
+            {props.type === 'textarea' && <textarea ref={ref} className="input-text" />}
+            {props.type === 'number' && <input ref={ref} type={props.type} className="input-text" value={props.value} min={props.min} max={props.max} />}
         </div>
     );
 };
 
-export default Input;
+export default forwardRef(Input);
--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,27 +1,56 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 import Input from '../components/Input';
 
-//Stylesheet
+const Contact = () => {
+    // Set up the refs we need
+    const nameRef = useRef(null);
+    const emailRef = useRef(null);
+    const messageRef = useRef(null);
 
-class Contact extends Component {
-    render() {
-        return (
-            <main id="contact-container">
-                <h1 className="header" style={{ marginBottom: '16px' }}>
-                    Contact me
-                </h1>
-                <form id="contact-form">
-                    <div className="flex space-between">
-                        <Input type="text" label="FULL NAME" />
-                        <Input type="text" label="EMAIL" />
-                    </div>
-                    <Input type="textarea" label="MESSAGE" />
+    const handleSubmit = () => {
+        // Create body content
+        const body = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            message: messageRef.current.value,
+        };
 
-                    <input type="button" id="contact-send" value="SEND" />
-                </form>
-            </main>
-        );
-    }
-}
+        // Send the request
+        fetch('/api/mail/send', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        })
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error(res.statusText + url);
+                } else {
+                    console.log('Email sent!');
+                }
+            })
+            .catch((err) => {
+                console.error('SERVER: Error sending message: ' + err);
+            });
+    };
+    return (
+        <main id="contact-container">
+            <h1 className="header" style={{ marginBottom: '16px' }}>
+                Contact me
+            </h1>
+            <form id="contact-form">
+                <div className="flex space-between">
+                    <Input type="text" label="FULL NAME" ref={nameRef} />
+                    <Input type="text" label="EMAIL" ref={emailRef} />
+                </div>
+                <Input type="textarea" label="MESSAGE" ref={messageRef} />
+
+                <input type="button" id="contact-send" value="Send Message" onClick={handleSubmit.bind(this)} />
+            </form>
+        </main>
+    );
+};
 
 export default Contact;
