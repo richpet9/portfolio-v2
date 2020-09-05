79a5b53dd2ea0bdfbb670e9f2604634b41a6f7c8
richpet9 <richpet9@gmail.com>
2020-09-05 11:58:48 -0400

    remvoed watch

--- pm2.config.js
+++ pm2.config.js
@@ -4,9 +4,6 @@ module.exports = {
             name: 'portfolio-v2',
             script: 'backend/server.js',
             node_args: '-r dotenv/config',
-            watch: ['frontend/public'],
-            watch_delay: 1000,
-            ignore_watch: ['node_modules']
-        }
-    ]
+        },
+    ],
 };
